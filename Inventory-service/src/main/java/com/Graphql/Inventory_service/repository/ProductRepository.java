3c21cf2890e781f8f4936b658d73c9f03bf888cf
FirstnameSatish <163973503+FirstnameSatish@users.noreply.github.com>
2024-07-05 12:36:46 -0500

    Add files via upload

---
+++ Inventory-service/src/main/java/com/Graphql/Inventory_service/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.Graphql.Inventory_service.repository;
+
+import com.Graphql.Inventory_service.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    List<Product> findByCategory(String category);
+}
